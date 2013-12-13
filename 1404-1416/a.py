8f46374fc688d50e0e89e286aea2f8e1704f708f
noatino <paranoid.tony@gmail.com>
2013-12-13 09:19:10 -0600

    Just another constants

--- 1404-1416/a.py
+++ 1404-1416/a.py
@@ -2,4 +2,12 @@
 #In this file I put the constans to use for b####################
 #################################################################
 
-c = 1 #Speed of light at the constant of plank
+import math
+
+c = 1  #Speed of light at the constant of plank
+cs = 1/(math.sqrt(3))
+mp = 1.67e-27 #mass of a proton
+me = 9.109e-31 #mass of an electron
+mn = 1.6726e-23 #mass of a neutron
+thetaC = math.sqrt(2/3)
+
