1bbea21e80e49446b95942a1a9a38107a8c28dfc
noatino <paranoid.tony@gmail.com>
2013-12-14 00:29:46 -0600

    Add the file c

---
+++ 1404-1416/c.py
@@ -0,0 +1,9 @@
+'''
+In this file we implement the functions and vars of the others 
+modules
+'''
+from a import *
+from b import *
+
+print(rs(r, gammaC))
+print(rph(Ec, sigmaT, thetaC, gammaC, mp, c))
