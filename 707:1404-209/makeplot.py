5a227fc18453ae5d7495e62d20314eb2063b39b1
noatino <paranoid.tony@gmail.com>
2014-01-13 02:15:13 -0600

    I need change the if

--- 707:1404-209/makeplot.py
+++ 707:1404-209/makeplot.py
@@ -5,29 +5,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def fEpsilonCo():
-    x1 = []
-    y1 = []
+    x = []
+    y = []
     epsilonPhCo = En(Ec, r_star, gammac,alphar, thetac)
     fEpsilonPhCo = fcon(Ec, r_star, gammac,alphar, thetac)
 
 
     for ii in np.arange(-1.0,3.0,0.1):
         epsilon = pow(10,ii)
-        x1.append(epsilon)
+        x.append(epsilon)
         if epsilon < epsilonPhCo:
-            y1.append(fEpsilonPhCo*pow(epsilon/epsilonPhCo,2.0))
+            y.append(fEpsilonPhCo*pow(epsilon/epsilonPhCo,2.0))
         if epsilon > epsilonPhCo:
-            y1.append(fEpsilonPhCo*pow(epsilon/epsilonPhCo,-1.0))
+            y.append(fEpsilonPhCo*pow(epsilon/epsilonPhCo,-1.0))
 
     try:
-        X = np.array(x1,float)
-        Y1 = np.array(y1,float)
-        print X#delete
-        print '-----------------------------------------------------------------------------------------------------------------------'#detele
-        print Y1#delete
+        X = np.array(x,float)
+        Y = np.array(y,float)
         rw1=plt.plot(X,Y1,color="green", linewidth=1.0, linestyle="-", label="E$_\pi$")
-        print 'all from plot 1'#delete
-
         a=plt.gca()
         a.set_yscale('log')
         a.set_xscale('log')
@@ -36,36 +31,38 @@ def fEpsilonCo():
         plt.title('')
         plt.show()
     except ValueError:
-        print 'Diferent values on the plots', sys.exc_info()[0]
+        print 'Diferent values on the plots'
 
         
 def fEpsilon():
-    x1 = []
-    y1 = []
-    epsilonCSC = 0.1
-    epsilonASC = 0.3
+    x = []
+    y = []
+    epsilonCSC = 0.1 #change this value to the trust
+    epsilonASC = 1e4 #change this value to the trust
     epsilonMSC = E_mssc(xiB,Lj, gammaj,dt)
     fEpsilonECSC = opt(Lj, z,gammaj, dt)*Fco(xiB,Lj, gammaj,dt,dz)
 
     for ii in np.arange(-1.0,3.0,0.1):
         epsilon = pow(10,ii)
-        x1.append(epsilon)
+        x.append(epsilon)
         if(epsilon < epsilonASC):
-            y1.append(fEpsilonECSC*pow(epsilonASC/epsilonCSC,1/3)*epsilon/epsilonASC)
+            y.append(fEpsilonECSC*pow(epsilonASC/epsilonCSC,1.0/3.0)*epsilon/epsilonASC)
+            break
         if(epsilonASC < epsilon and epsilon < epsilonCSC):
-            y1.append(fEpsilonECSC*pow(epsilon/epsilon,1/3))
+            y.append(fEpsilonECSC*pow(epsilon/epsilonCSC,1.0/3.0))
+            break
         if(epsilonCSC < epsilon and epsilon < epsilonMSC):
-            y1.append(fEpsilonECSC*pow(epsilon/epsilonCSC,-1/2))
+            y.append(fEpsilonECSC*pow(epsilon/epsilonCSC,-1.0/2.0))
+            break
         if(epsilonMSC < epsilon):
-            y1.append(fEpsilonECSC*pow(epsilonMSC/epsilonCSC,-1/2)*pow(epsilon/epsilonMSC,-1/2))
+            y.append(fEpsilonECSC*pow(epsilonMSC/epsilonCSC,-1.0/2.0)*pow(epsilon/epsilonMSC,-1.0/2.0))
+            break
+        y.append(0)
+
     try:
-        X = np.array(x1,float)
-        Y1 = np.array(y1,float)
-        print X#delete
-        print '-----------------------------------------------------------------------------------------------------------------------'#delete
-        print Y1#delete
-        rw1=plt.plot(X,Y1,color="green", linewidth=1.0, linestyle="-", label="E$_\pi$")
-        print 'all from plot2'#delete
+        X = np.array(x,float)
+        Y = np.array(y,float)
+        rw1=plt.plot(X,Y,color="green", linewidth=1.0, linestyle="-", label="E$_\pi$")
         
         a=plt.gca()
         a.set_yscale('log')
@@ -75,4 +72,7 @@ def fEpsilon():
         plt.title('')
         plt.show()
     except ValueError:
-        print 'Diferent values on the plots', sys.exc_info()[0]
+        print 'Diferents values of the dimentions of x and y'
+        raise    
+
+fEpsilon()
