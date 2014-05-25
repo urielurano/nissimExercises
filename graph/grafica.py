dbdef7eb6f612cc8ac42668cdbc1231792c64a08
noatino <paranoid_14@hotmail.com>
2014-05-24 21:59:22 -0500

    update

--- graph/grafica.py
+++ graph/grafica.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 
 def makePlot(listaDePuntos):
     plt.figure()
+    colors = ['b','g','r','c','m','y','k','w']
     for x in range(0, len(listaDePuntos)):
         try:
-            Xtmp, Ytmp = np.loadtxt(listaDePuntos[x], unpack = True)
-            plt.plot(Xtmp, Ytmp,'*')
+            X, Y, Xizq, Xder, Yarr, Yaba = np.loadtxt(listaDePuntos[x], unpack = True)
+            plt.errorbar(X,Y, Yarr, Yaba, linestyle="none", marker="*", markersize=4.0, capsize=3.0, label = str(x+1))
+            a=plt.gca()
+            a.set_yscale('log')
+            a.set_xscale('log')
             plt.savefig('graph.png')
-            #a=plt.gca()
-            #a.set_yscale('log')
-            #a.set_xscale('log')
         except:
             print 'Hay un error, revisa:'
             raise
@@ -25,5 +26,5 @@ puntos11 = ['points11-1.dat','points11-2.dat','points11-3.dat']
 puntos12 = ['points12-1.dat','points12-2.dat','points12-3.dat','points12-4.dat','points12-5.dat','points12-6.dat','points12-7.dat','points12-8.dat']
 
 
-makePlot(puntos11)
+#makePlot(puntos11)
 makePlot(puntos12)
